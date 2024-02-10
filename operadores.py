049706099d96a988bb0df098d60d24b225b2cd96
evelyntecinternet <evelynacoliveira06@gmail.com>
2024-02-10 14:28:26 -0300

    3-commit

---
+++ operadores.py
@@ -0,0 +1,30 @@
+"""
+Soma: +
+Subtração: -
+Multiplicação: *
+Divisão: /
+Divisão inteira: //
+Resto da divisão: %
+Potência: **
+"""
+numero1 = 10
+numero2 = 20
+print(numero1 + numero2)
+print(numero1 - numero2)
+print(numero1 * numero2)
+print(numero1 / numero2)
+print(20 % 3)
+print(2 ** 3)
+
+# Operadores Booleanos
+idade1 = 10
+idade2 = 15
+altura1 = 1.77
+altura2 = 1.65
+altura3 = altura2
+print(idade1 > idade2) # False
+print(idade1 <= idade2) # True
+print("Python" == "python") # False
+print("Banana" != "abacate") # True
+print(altura1 >= altura2) # True
+print(altura2 > altura3) # False
