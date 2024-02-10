5e575a800193837f06b8a62ec959bf8d6365e539
evelyntecinternet <evelynacoliveira06@gmail.com>
2024-02-10 14:14:15 -0300

    variaveis_Commit

---
+++ variaveis.py
@@ -0,0 +1,28 @@
+idade = 26 #criando váriavel
+print(idade)
+
+nome = "Evelyn Oliveira"
+print(nome)
+'''
+Tpos de váriaveis
+1 - int = número inteiro,1,2,-20,-1000
+2 - str = texto,cadeias de caracteres
+3 - float = número real,1.2,4.5,-9.1
+4 - bool(booleanos) = valores lógicos; True ou False
+'''
+idade = 26
+altura = 1.62
+nome = "Evelyn Oliveira"
+estudando = True #sempre maísculo
+
+print(type(idade))
+print(type(altura))
+print(type(nome))
+print(type(estudando))
+
+# obtendo dados do usuário e salvando em variáveis
+
+linguagem = input("Qual é a linguagem de programação que você está estudando?")
+print("A linguagem que você está estudando é ",linguagem)
+
+# Imprimindo variáveis + Mais sobre a função print
