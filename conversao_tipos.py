49b47ea8e9c85577ec050056af58243a24344b54
evelyntecinternet <evelynacoliveira06@gmail.com>
2024-02-10 14:35:51 -0300

    convencao_tipos

---
+++ conversao_tipos.py
@@ -0,0 +1,12 @@
+idade = '26'
+numero1 = '10'
+numero2 = '20'
+print(idade,type(idade))
+idade_inteira = int(idade)
+print(idade_inteira,type(idade_inteira))
+# int()
+# str()
+# float()
+# bool()
+altura = float (input("Informe a sua altura: "))
+print(altura,type(altura))
