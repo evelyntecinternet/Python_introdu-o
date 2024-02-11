c3865f991e6c42c8d8c0556504f91aea12a18c37
evelyntecinternet <evelynacoliveira06@gmail.com>
2024-02-11 11:20:05 -0300

    commit

--- condicionais.py
+++ condicionais.py
@@ -8,6 +8,7 @@ else:
 Imagine que você queira imprimir "aprovada(o)", caso o estudante tenha uma média 
 superior ou igual a 7 ,e "Reprovado(a)",caso a média seja inferior a 7.  
 """
+'''
 media = float(input("Informe a média do estudante: "))
 if media >= 7:
     print("Você foi aprovado(a)!")
@@ -15,3 +16,11 @@ elif media >= 5:
     print("Recuperação!")
 else:
     print("Você foi reprovado(a)!")
+    '''
+media = 10
+presenca = 100
+if media >= 7 and presenca >= 70:
+    print("Aprovado!")
+    print("Parabéns!")
+else:
+    print("Reprovado!")    
